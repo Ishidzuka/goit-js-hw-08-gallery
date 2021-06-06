f245a44f7239644a3b7c904d9285ea58d05d8c7c
Ishidzuka <77970240+Ishidzuka@users.noreply.github.com>
2021-06-06 19:10:39 +0300

    hw-08 finished

--- js/gallery.js
+++ js/gallery.js
@@ -7,14 +7,44 @@ const ref = {
     modalButtonRef: document.querySelector('.lightbox__button'),
 }
 
-const createGallery = gallery.map(({ preview, original, description }, element) =>
+const createGallery = gallery.map(({ preview, original, description }, index) =>
 `
 <li class="gallery__item">
 <a class="gallery__link" href=" ">
-<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" data-index="${element}">
+<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" data-index="${index}">
 </a>
 </li>
 `).join('');
 console.log(createGallery);
 
 ref.jsGalleryRef.innerHTML = createGallery;
+
+const outerModalClick = el => {
+    el.preventDefault();
+
+    if (el.target.localName === 'img') {
+        ref.modalImgRef.src = el.target.dataset.source;
+        ref.modalImgRef.alt = el.target.alt;
+        ref.modalImgRef.dataset.index = el.target.index;
+        ref.modalRef.classList.add('is-open');
+    }
+};
+
+const closeKey = el => {
+    if (el.key === "Escape") {
+        ref.modalRef.classList.remove('is-open');
+    }
+};
+
+const closeModalClick = el => {
+    if (el.target.localName !== 'img'){
+       ref.modalRef.classList.remove('is-open') 
+        ref.modalImgRef.src = '';
+        ref.modalImgRef.alt = '';
+    }
+}
+
+ref.jsGalleryRef.addEventListener('click', outerModalClick);
+window.addEventListener('keyup', closeKey);
+window.addEventListener('click', closeModalClick);
+
